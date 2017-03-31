aca59cd796922dcc1a0fa4892ec56fc523f8c7a9
Francois <francois.manlay@gmail.com>
2017-03-31 16:14:59 +0200

    Add files via upload

--- Histograms.py
+++ Histograms.py
@@ -117,6 +117,67 @@ plt.savefig("D:/work/github/horizontal_histgrm_2.png")
 plt.show()
 
 
+# In[22]:
+
+# improvement form for vertical histogram
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt ('D:/work/dyna_tempo.txt', delimiter="\t", skip_header=1)
+patents = np.array(data[:,1], int)
+years = np.array(data[:,0], int)
+
+freq_series = pd.Series.from_array(patents) 
+
+# now to plot the figure... we have to use plt.figure method because we have 2 information years and patents
+# this goes through plt.figure method and ax. method (this is no longer a simple plot but a 2-axis-information plot)
+plt.figure(figsize=(8, 4)) # adjusting graph size / proportions
+ax = freq_series.plot(kind='bar', label='Volume', color='lightblue')
+ax.set_title("title", fontweight='bold', size=15)
+ax.set_xlabel("x label", size = 13)
+ax.set_ylabel('y label', size = 13)
+ax.set_xticklabels(years) # tickles for X axis
+ax.grid(False) #no grid
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+
+rects = ax.patches
+
+# Rotating the xticklabels for years
+for label in ax.get_xmajorticklabels():
+    label.set_rotation(45)
+    label.set_horizontalalignment("right")
+
+# Making the labels
+def autolabel(rects, ax):
+    # Get y-axis height to calculate label position from. 
+    (y_bottom, y_top) = ax.get_ylim()
+    y_height = y_top - y_bottom
+
+    for rect in rects:
+        height = rect.get_height()
+
+        # Fraction of axis height taken up by this rectangle
+        p_height = (height / y_height)
+
+        # If we can fit the label above the column, do that;
+        # otherwise, put it inside the column.
+        if p_height > 0.95: # arbitrary; 95% looked good to me.
+            label_position = height - (y_height * 0.1)
+        else:
+            label_position = height + (y_height * 0.01)
+
+        ax.text(rect.get_x() + rect.get_width()/2., label_position,
+                '%d' % int(height), ha='center', va='bottom', rotation=-10, fontsize=8)
+
+autolabel(rects, ax)
+plt.tight_layout()
+plt.legend(loc='upper center')
+plt.savefig("D:\work\github\horizontal_histgrm_3.png")
+plt.show()
+
+
 # In[ ]:
 
 
